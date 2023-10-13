4d8f3f19eff7b37f93a5077fb2cea202ec227f8d
Lemuel <lemuelpuglisi001@gmail.com>
2023-10-13 10:38:39 +0200

    feat: resume process

--- turboprep-multiple.py
+++ turboprep-multiple.py
@@ -1,7 +1,11 @@
 import os
 import argparse
+import numpy as np
 import nibabel as nib
+from multiprocessing import Pool
 from tqdm import tqdm
+from intensity_normalization.normalize.whitestripe import WhiteStripeNormalize
+from intensity_normalization.typing import Modality
 
 NPROC = os.cpu_count()
 
@@ -35,6 +39,8 @@ if __name__ == '__main__':
     assert os.path.exists(template), 'template image file doesn\'t exist'
     assert nbc_file is None or os.path.exists(nbc_file), 'no-bfc file doesn\'t exist'
 
+    print('🚀 reading input files')
+
     with open(inp_file, 'r') as f:
         inp_list = [ l.strip() for l in f.readlines() ]
 
@@ -46,85 +52,190 @@ if __name__ == '__main__':
         with open(nbc_file, 'r') as f:
             nbc_list = set([ l.strip() for l in f.readlines() ])
 
-    inp_out_pairs = zip(inp_list, out_list)
-    reg_seg_pairs = []
+
+    print('🚀 creating output dictionary')
+
+    outputs_dict = {}
+    for input_path, output_path in tqdm(zip(inp_list, out_list), total=len(inp_list)):
+
+        if not os.path.exists(input_path):
+            print('file', input_path, 'does not exists.')
+            continue
+        elif not os.path.exists(output_path):
+            os.makedirs(output_path)
+
+        outputs_dict[input_path] = {
+            'bias_field_correction':    os.path.join(output_path, 'corrected.nii.gz'),
+            'ants_prefix':              os.path.join(output_path, 'turboprep_'),
+            'affine_registration':      os.path.join(output_path, 'turboprep_Warped.nii.gz'), 
+            'semantic_segmentation':    os.path.join(output_path, 'segm.nii.gz'),
+            'brain_mask_extraction':    os.path.join(output_path, 'mask.nii.gz'), 
+            'intensity_normalization':  os.path.join(output_path, 'normalized.nii.gz'), 
+            'brain_extraction':         os.path.join(output_path, 'brain.nii.gz')
+        }
+
+        if input_path in nbc_list:
+            outputs_dict[input_path]['bias_field_correction'] = input_path
 
     #######################################################
     # Bias field correction and registration to template  #
     #######################################################
 
-    for inp_path, out_path in tqdm(inp_out_pairs, total=len(inp_list)):
+    print('🚀 Bias-field correction and registration to template')
+
+    for input_path in tqdm(list(outputs_dict.keys())):
         
-        if not os.path.exists(inp_path):
-            print('File not exists, skipping', inp_path)
+        if input_path == '/media/data/datasets/mri_database/ADNI/subjects/082_S_4224/I1051042.nii.gz':
+            print('skipping file')
             continue
 
-        if not os.path.exists(out_path):
-            os.makedirs(out_path)
+        input_outputs = outputs_dict[input_path]
+        corrected_path  = input_outputs['bias_field_correction']
+        registered_path = input_outputs['affine_registration']
+        registered_pref = input_outputs['ants_prefix']
+        brain_path = input_outputs['brain_extraction']
 
-        corrected_path = inp_path
-        if inp_path not in nbc_list:
-            # apply bias field correction.
-            corrected_path = os.path.join(out_path, 'corrected.nii.gz')
-            os.system(f"N4BiasFieldCorrection -d 3 -i {inp_path} -o {corrected_path} -s {shrinkf} -v  > /dev/null")
+        if os.path.exists(registered_path) or os.path.exists(brain_path):
+            # if the registered path exists, then we have already done 
+            # this step. If does not exists, then we could have performed 
+            # the brain extraction, after which the registered scan id
+            # removed, so check for the file with the brain extracted.
+            continue
 
-        reg_output = os.path.join(out_path, 'turboprep_')
+
+        if input_path != corrected_path:
+            os.system('N4BiasFieldCorrection -d 3 '
+                     f'-i {input_path} ' 
+                     f'-o {corrected_path} '
+                     f'-s {shrinkf} -v > /dev/null')
+
+        if not os.path.exists(corrected_path):
+            print('N4 correction has failed.')
+            del outputs_dict[input_path]
+            continue
+
         os.system('antsRegistrationSyNQuick.sh -d 3 '
                   f'-f {template} -m {corrected_path} ' 
-                  f'-o {reg_output} -n {threads} '
-                  f'-t {regtype}  > /dev/null')
-
-        if not keepint:
-            os.remove(os.path.join(out_path, 'turboprep_InverseWarped.nii.gz'))
-            if corrected_path != inp_path: os.remove(corrected_path)
+                  f'-o {registered_pref} -n {threads} '
+                  f'-t {regtype} > /dev/null')
         
-        os.rename(os.path.join(out_path, 'turboprep_0GenericAffine.mat'),
-                  os.path.join(out_path, 'affine_transf.mat'))
+        if not os.path.exists(registered_path):
+            print('Affine registration has failed.')
+            del outputs_dict[input_path]
+            continue
 
-        reg_path = os.path.join(out_path, 'turboprep_Warped.nii.gz')
-        reg_seg_pairs.append((reg_path, os.path.join(out_path, 'segm.nii.gz')))
+        else:
+            if not keepint:
+                os.remove(registered_pref + 'InverseWarped.nii.gz')
+                if corrected_path != input_path: os.remove(corrected_path)
+            os.rename(registered_pref + '0GenericAffine.mat', 
+                      os.path.join(os.path.dirname(registered_pref), 'affine_transf.mat'))
 
-    
+
     #######################################################
     # Semantic segmentation with SynthSeg                 #
     #######################################################
 
-    with open('temp-input.txt', 'w') as f:
-        for reg, _ in reg_seg_pairs:
-            f.write(reg + '\n')
+    print('🚀 semantic segmentation using SynthSeg')
 
-    with open('temp-output.txt', 'w') as f:
-        for _, out in reg_seg_pairs:
-            f.write(out + '\n')
+    reg_seg_pairs = []
+    for input_path, input_dict in outputs_dict.items():
+        reg_path = input_dict['affine_registration']
+        seg_path = input_dict['semantic_segmentation']
+        if not os.path.exists(seg_path):
+            reg_seg_pairs.append((reg_path, seg_path))
 
-    os.system('mri_synthseg '
-              f'--i temp-input.txt ' 
-              f'--o temp-output.txt '
-              f'--fast '
-              f'--threads {threads}')
+    if len(reg_seg_pairs) > 0:
 
-    os.remove('temp-input.txt')
-    os.remove('temp-output.txt')
+        if os.path.exists('temp-input.txt'):  os.remove('temp-input.txt')
+        if os.path.exists('temp-output.txt'): os.remove('temp-output.txt')
+
+        with open('temp-input.txt', 'w') as f:
+            for reg, _ in reg_seg_pairs:
+                f.write(reg + '\n')
+
+        with open('temp-output.txt', 'w') as f:
+            for _, seg in reg_seg_pairs:
+                f.write(seg + '\n')
+
+        os.system('mri_synthseg '
+                f'--i temp-input.txt ' 
+                f'--o temp-output.txt '
+                f'--fast '
+                f'--threads {threads}')
+
+        os.remove('temp-input.txt')
+        os.remove('temp-output.txt')
+
+    for input_path in list(outputs_dict):
+        segm_path = outputs_dict[input_path]['semantic_segmentation']
+        if not os.path.exists(segm_path):
+            print('failed segmentation on', input_path)
+            del outputs_dict[input_path]
 
     #######################################################
     # Brain extraction and intensity normalization         #
     #######################################################
 
-    for reg_path, seg_path in tqdm(reg_seg_pairs):
-        
+    def mask_and_normalize(paths):
+        reg_path, seg_path = paths
         output_dir = os.path.dirname(seg_path)
-        mask_path = os.path.join(output_dir, 'mask.nii.gz')
-        norm_path = os.path.join(output_dir, 'normalized.nii.gz')
+        mask_path  = os.path.join(output_dir, 'mask.nii.gz')
+        norm_path  = os.path.join(output_dir, 'normalized.nii.gz')
+        brain_path = os.path.join(output_dir, 'brain.nii.gz')
 
-        os.system(f'mri_threshold -B 1 {seg_path} 2 {mask_path} > /dev/null')
-        os.system(f'ws-normalize -m {mask_path} -o {norm_path} -mo {modality} {reg_path} > /dev/null')
+        if os.path.exists(mask_path) and \
+           os.path.exists(norm_path) and \
+           os.path.exists(brain_path): return
 
-        if not keepint:
+        try:
+            reg = nib.load(reg_path)
+            seg = nib.load(seg_path)
+            reg_arr = reg.get_fdata()
+        except:
+            print('loading failed for', reg_path)
+            return
+
+
+        if not os.path.exists(mask_path):
+            try:
+                mask_arr = (seg.get_fdata().round() > 0).astype(np.uint8)
+                mask = nib.Nifti1Image(mask_arr, seg.affine, seg.header)
+                mask.to_filename(mask_path)
+            except:
+                print('brain extraction failed for', reg_path)
+                return
+
+        if not os.path.exists(norm_path):
+            try:
+                ws_norm = WhiteStripeNormalize()
+                normalized_arr = ws_norm(reg_arr, mask_arr, modality=Modality.T1)
+                normalized = nib.Nifti1Image(normalized_arr, reg.affine, reg.header)
+                normalized.to_filename(norm_path)
+            except:
+                print('normalization failed for', reg_path)
+                return
+
+        if not os.path.exists(brain_path):
+            try:
+                brain_arr = normalized_arr.copy()
+                brain_arr[ mask_arr == 0. ] = brain_arr.min()
+                brain = nib.Nifti1Image(brain_arr, reg.affine, reg.header)
+                brain.to_filename(brain_path)
+            except:
+                print('brain extraction failed for', reg_path)
+                return
+        
+        if os.path.exists(reg_path):
             os.remove(reg_path)
 
-        smri = nib.load(norm_path)
-        mask = nib.load(mask_path).get_fdata().round()
-        smri_arr = smri.get_fdata()
-        smri_arr[mask == 0] = smri_arr.min()        
-        brain = nib.Nifti1Image(smri_arr, smri.affine, smri.header)
-        nib.save(brain, os.path.join(output_dir, 'brain.nii.gz'))
+
+    print('🚀 computing brain mask, intensity normalization and skull stripping')
+
+    reg_seg_pairs = [ (d['affine_registration'], d['semantic_segmentation']) for d in outputs_dict.values() ]
+
+    pool = Pool(processes=threads)
+    for _ in tqdm(pool.imap_unordered(mask_and_normalize, reg_seg_pairs), total=len(reg_seg_pairs)):
+        pass
+
+    print('🚀 finish.')
